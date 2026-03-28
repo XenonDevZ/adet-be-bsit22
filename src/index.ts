498f74d392156aa5efde2c94345e2ae8c8ed70f8
razz.ernesto@gmail.com <ernesto@muuvment.com>
2026-03-28 09:40:52 +0800

    feat: add and configure CORS middleware for the Hono application.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 import postsRoute from './routes/posts.route';
+import { cors } from 'hono/cors';
 
 const app = new Hono()
+
+app.use(cors({
+  origin: 'http://localhost:4200',
+  allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowHeaders: ['Content-Type', 'Authorization'],
+  exposeHeaders: ['Content-Length'],
+  maxAge: 600,
+  credentials: true,
+}));
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
